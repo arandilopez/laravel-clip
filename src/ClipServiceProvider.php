cc9f7e64c5ecf44ce3663f71311b6b5274a2cbeb
Arandi López <arandilopez.93@gmail.com>
2017-03-17 14:27:09 -0600

    initial commit

---
+++ src/ClipServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Clip;
+
+use Illuminate\Support\ServiceProvider;
+
+class ClipServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        # code...
+    }
+
+    public function boot()
+    {
+        # code...
+    }
+}
