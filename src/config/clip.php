ebb0721abcf81287c3cee99b79ad493706683ccd
Arandi López <arandilopez.93@gmail.com>
2017-03-20 12:57:06 -0600

    clip config file

---
+++ src/config/clip.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    /**
+     *
+     */
+    'model' => 'Clip\Model\Attachment',
+
+    /**
+     *
+     */
+    'soft_deletes' => false,
+
+    /**
+     *
+     */
+    'disk' => 'local',
+
+    /**
+     *
+     */
+    'basedir' => 'attachments',
+];
